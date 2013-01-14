3e311b0ddf5536bcc607c3fa7c9db38422f40a3c
Jay McCarthy <jay.mccarthy@gmail.com>
2013-01-14 14:13:29 -0700

    Category links

--- post.rkt
+++ post.rkt
@@ -30,8 +30,11 @@
             #:tag tag
             (format "~a-~a-~a: " year month day)
             content ...)
-           @sb:margin-note{The source for this post is online at @sb:link[(format "https://github.com/jeapostrophe/jeapostrophe.github.com/tree/source/posts/~a" fname)]{@|fname|}})))]))
-(define (categories . _) (void))
+           @sb:margin-note{The source for this post is online at @sb:link[(format "https://github.com/jeapostrophe/jeapostrophe.github.com/tree/source/posts/~a" fname)]{@|fname|}.})))]))
+
+(define (categories . l)
+  @sb:t{@sb:bold{Categories:} @(map (λ (c) @sb:elem{@sb:secref[c] }) l)})
+
 (define (the-jump . _) (void))
 
 (provide (all-defined-out))
