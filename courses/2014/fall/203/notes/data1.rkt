72120fb278410f60d275862a039d7f6a61eab85f
Jay McCarthy <jay.mccarthy@gmail.com>
2014-09-19 10:59:01 -0400

    Update

--- courses/2014/fall/203/notes/data1.rkt
+++ courses/2014/fall/203/notes/data1.rkt
@@ -74,7 +74,37 @@
     [(leaf)
      u]
     [(branch l h r)
-     (diff l (diff r (remove u h)))]))
+     ;; Good
+     (diff l (diff r (remove u h)))
+     ;; An error
+     #;
+     (define up (remove u h))
+
+     ;; T = L u R + {h}
+     ;; U - T = U - (L u R + {h})
+
+     ;; Suppose that U = L u R
+
+     ;; (u ((U - {h}) - L)
+     ;;    ((U - {h}) - R))
+
+     ;; (u ((L u R - {h}) - L)
+     ;;    ((L u R - {h}) - R))
+
+     ;; (u (R - {h})
+     ;;    (L - {h}))
+
+     ;; (R u L) - {h}
+
+     ;; (L u R) - {h}
+
+     ;; U - {h}
+
+     ;; up
+     #;
+     (union (diff l up)
+            (diff r up))
+     ]))
 (define (equal t u)
   (and (subset t u)
        (subset u t)))
@@ -88,24 +118,28 @@
           (subset r u))]))
 
 (module+ test
-  (define COUNT 100)
+  (define COUNT 1000)
 
   (define (set)
     (if (zero? (random 2))
       (empty)
       (add (set) (int))))
   (define (int)
-    (random COUNT))
+    (random 10))
 
   (define-syntax-rule (forall ([var gen] ...) prop ...)
     (for ([i (in-range COUNT)])
       (define var (gen))
       ...
-      (unless prop
-        (printf "Property ~v fails when\n" 'prop)
-        (printf "\t~a = ~v\n"
-                'var var)
-        ...)
+      (cond
+        [prop
+         (printf ".")]
+        [else
+         (printf "Property ~v fails when\n" 'prop)
+         (printf "\t~a = ~v\n"
+                 'var var)
+         ...
+         (exit 1)])
       ...))
 
   (define (<-> x y)
@@ -114,20 +148,49 @@
     (or (not x) y))
 
   (forall
-   ([t set] [u set] [x int] [y int])
+   ([t set] [u set] [v set] [x int] [y int])
    (<-> (member (add t x) y)
         (or (= x y) (member t y)))
-   (<-> (member (union t u) x)
-        (or (member t x)
-            (member u x)))
-   (or (= (cardinality (add t x))
-          (cardinality t))
-       (= (cardinality (add t x))
-          (+ 1 (cardinality t))))
-   (isEmptyHuh (empty))
-   (not (isEmptyHuh (add t x)))
-   (<-> (isEmptyHuh t)
-        (zero? (cardinality t)))
-   (-> (not (member t x))
-       (equal (remove (add t x) x) t))))
+   (-> (isEmptyHuh t)
+       (not (member t x)))
+   (-> (subset t u)
+       (<= (cardinality t)
+           (cardinality u)))
+   (-> (equal t u)
+       (= (cardinality t)
+          (cardinality u)))
+   ;; -> isn't exactly if
+   #;
+   (-> (not (isEmptyHuh t))
+       (not (-> (= (cardinality t)
+                   (cardinality u))
+                (equal t u))))
+   (-> (= (cardinality t)
+          (cardinality u))
+       (<-> (equal t u)
+            (isEmptyHuh (diff t u))))
 
+   (-> (and (equal u (union t v))
+            (not (equal t v)))
+       (equal u
+              (union (diff t u) (diff v u))))
+
+   (equal (union (inter t u) 
+                 (union (diff t u) 
+                        (diff u t)))
+          (union t u))
+   
+   ;; (<-> (member (union t u) x)
+   ;;      (or (member t x)
+   ;;          (member u x)))
+   ;; (or (= (cardinality (add t x))
+   ;;        (cardinality t))
+   ;;     (= (cardinality (add t x))
+   ;;        (+ 1 (cardinality t))))
+   ;; (isEmptyHuh (empty))
+   ;; (not (isEmptyHuh (add t x)))
+   ;; (<-> (isEmptyHuh t)
+   ;;      (zero? (cardinality t)))
+   ;; (-> (not (member t x))
+   ;;     (equal (remove (add t x) x) t))
+   ))
