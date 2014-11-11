217a1f19297750f83c6124a1b1dad3c71900833e
Jay McCarthy <jay.mccarthy@gmail.com>
2014-11-11 13:40:07 -0500

    Update

---
+++ courses/2014/fall/203/notes/28.java
@@ -0,0 +1,34 @@
+class C28 {
+    public static void main( String[] args ) {
+    }
+}
+
+// Validation
+// - Verification
+// - Testing
+
+// Verification
+// - Theorems
+// - Informal
+
+// Testing
+// - Black box: Based on spec
+// - Glass box: Based on code
+
+// Glass
+// - Aliasing
+// - Boundaries
+
+// Testable code
+// - Loops or no?
+// - Small functions
+// - More parameters
+// - More returns
+// - Error early
+
+// Bugs vs Errors
+
+// Finding errors
+// - Build and expand your knowledge
+// - Small problems better than big problems
+
