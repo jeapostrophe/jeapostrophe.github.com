5f73d5cafa92ed35f137fa5a62b916b95d437793
Jay McCarthy <jay.mccarthy@gmail.com>
2015-10-06 13:31:32 -0400

    Update

--- courses/2015/fall/305/notes/dist/a4_mult_test.asm
+++ courses/2015/fall/305/notes/dist/a4_mult_test.asm
@@ -1,29 +1,36 @@
 ; test for the new MULT assembly instruction
-start:	LODD d1:
+start:
+	LODD d1:
 	PUSH
 	MULT 0
 	HALT	     ; AC has status, product on stack
-        LODD d2:
-        PUSH
-        MULT 37
-        HALT         ; AC has status, product on stack
-        LODD d3:
-        PUSH
-        MULT 63
-        HALT         ; AC has status, product on stack
-        LODD d4:
-        PUSH
-        MULT 16
-        HALT         ; AC has status, product on stack
-        LODD d5:
-        PUSH
-        MULT 49
-        HALT         ; AC has status, product on stack
-        LODD d6:
-        PUSH
-        MULT 55
-        HALT         ; AC has status, product on stack
-	.LOC 30
+    INSP 1
+    LODD d2:
+    PUSH
+    MULT 37
+    HALT         ; AC has status, product on stack
+    INSP 1
+    LODD d3:
+    PUSH
+    MULT 63
+    HALT         ; AC has status, product on stack
+    INSP 1
+    LODD d4:
+    PUSH
+    MULT 16
+    HALT         ; AC has status, product on stack
+    INSP 1
+    LODD d5:
+    PUSH
+    MULT 49
+    HALT         ; AC has status, product on stack
+    INSP 1
+    LODD d6:
+    PUSH
+    MULT 55
+    HALT         ; AC has status, product on stack
+    INSP 1
+.LOC 30
 d1:	 2542
 d2:	 -362
 d3:	  520
